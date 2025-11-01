194fa503a321e0ce067fbad97fa57c5a29497a0c
Md. Shariar Islam Mrida <shariarmrida03@gmail.com>
2025-11-01 16:56:25 +0600

    circleWithDatae

--- JavaOOP/Circle.java
+++ JavaOOP/Circle.java
@@ -1,20 +1,24 @@
 package JavaOOP;
-
+import java.util.Date;
 public class Circle {
     private double radius;
     private static int numberOfObject=0;
     public static final double pi = 3.1416;
+    Date dateCreated;
     Circle(){
         this.radius=0.0;
         numberOfObject++;
+        dateCreated= new Date();
     }
     Circle(double radius){
         this.radius=radius;
         numberOfObject++;
+        dateCreated= new Date();
     }
     Circle(Circle c){
         this.radius=c.radius;
         numberOfObject++;
+        dateCreated= new Date();
     }
     double getRadius(){
         return this.radius;
@@ -33,6 +37,7 @@ public class Circle {
     }
     @Override
     public String toString(){
-        return "Radius : "+radius+" Area : "+getArea();
+        return "Radius : "+radius+" Area : "+getArea()+"Number of ObjectCreated : "
+             + getNumberOfObject()+ "Date created" + dateCreated;
     }
 }
